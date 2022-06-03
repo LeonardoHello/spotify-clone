d030f239044628564fc84e26b67cfe3869acae40
Leonardo <Leonardo.Yakub@gmail.com>
2022-06-03 22:05:42 +0200

    done

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,6 @@
 const Login = () => {  
   const CLIENT_ID = '5cbba53ee72d441981c202e410cecc34';
-  const REDIRECT_URI = 'https://spotify-clone-react-project.herokuapp.com/';
+  const REDIRECT_URI = 'https://spotify-clone-react-project.herokuapp.com';
   const scope = 'streaming user-read-email user-read-private user-library-read user-library-modify user-read-playback-state user-modify-playback-state'.split(' ').join('%20');
 
   const authUrl = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&scope=${scope}`
